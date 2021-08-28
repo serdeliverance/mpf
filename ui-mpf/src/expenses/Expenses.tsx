e6d5e368a0c026a04826578727e15a783b3b722c
Sergio Cano <canosergio90@gmail.com>
2021-08-28 19:02:21 -0300

    av

--- ui-mpf/src/expenses/Expenses.tsx
+++ ui-mpf/src/expenses/Expenses.tsx
@@ -1,9 +1,26 @@
-import { Button } from '@material-ui/core';
+import {
+  Table,
+  TableCell,
+  TableHead,
+  TableRow,
+} from '@material-ui/core';
+
 import React from 'react';
 
 function Expenses() {
   return (
-    <Button color="primary">Expenses</Button>
+    <Table arial-label="simple table">
+      <TableHead>
+        <TableRow>
+          <TableCell>Name</TableCell>
+          <TableCell>Description</TableCell>
+          <TableCell>Category</TableCell>
+          <TableCell>Subcategories</TableCell>
+          <TableCell>Date</TableCell>
+          <TableCell>Amount</TableCell>
+        </TableRow>
+      </TableHead>
+    </Table>
   );
 }
 
