0f44b79a2e3e34de4cfb1e5352b0dc31d2c2b9f3
Sergio Cano <canosergio90@gmail.com>
2021-08-31 19:58:20 -0300

    mock api expenses

--- mock-api/src/index.ts
+++ mock-api/src/index.ts
@@ -1,15 +1,17 @@
-import { Request, Response } from 'express'
+import express from 'express'
 import { expenses } from './mocks/expenses'
 
-const express = require('express')
 const app = express()
 
+const router = express.Router()
+
 app.use(express.json())
+app.use('/api/v1', router)
+
 
 const port = 5000
 
-// TODO parameterize api version to avoid harding it in every endpoint
-app.get('/api/v1/expenses', (req: Request, res: Response) => {
+router.get('/expenses', (req, res) => {
     return res.status(200).send(expenses)
 })
 
