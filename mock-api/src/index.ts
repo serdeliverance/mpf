40397b946eb630dc09166aad1da302e77f89c6ea
Sergio Cano <canosergio90@gmail.com>
2021-08-31 22:34:32 -0300

    av

--- mock-api/src/index.ts
+++ mock-api/src/index.ts
@@ -1,17 +1,19 @@
 import express from 'express'
 import { expenses } from './mocks/expenses'
+import cors from 'cors'
 
 const app = express()
 
 const router = express.Router()
 
 app.use(express.json())
+app.use(cors)
 app.use('/api/v1', router)
 
 
 const port = 5000
 
-router.get('/expenses', (req, res) => {
+router.get('/expenses/:userId', (req, res) => {
     return res.status(200).send(expenses)
 })
 
