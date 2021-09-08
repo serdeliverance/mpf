4fa1644d3fd77379f4494a95ef22d7172d3ac132
Sergio Cano <canosergio90@gmail.com>
2021-09-08 18:56:54 -0300

    removeing ts - initial

--- ui-mpf/src/services/ExpenseService.js
+++ ui-mpf/src/services/ExpenseService.js
@@ -1,4 +1,4 @@
-export default async function getExpenses(month: number, year: number) {
+export default async function getExpenses(month, year) {
   // TODO remove hardcoded user
   const resp = await fetch(`${process.env.REACT_APP_EXPENSES_API_URL}/25?month=${month}&year=${year}`)
   if (!resp.ok) {
