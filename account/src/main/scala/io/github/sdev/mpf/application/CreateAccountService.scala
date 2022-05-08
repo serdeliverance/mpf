66241347a80a49230944763a00d8516caac439e5
Sergio Cano <canosergio90@gmail.com>
2022-05-07 21:16:08 -0300

    av
    Signed-off-by: Sergio Cano <canosergio90@gmail.com>

--- account/src/main/scala/io/github/sdev/mpf/application/CreateAccountService.scala
+++ account/src/main/scala/io/github/sdev/mpf/application/CreateAccountService.scala
@@ -6,11 +6,14 @@ import io.github.sdev.mpf.application.ports.out.AccountRepository
 import cats.ApplicativeError
 import cats.data.Validated.Valid
 import cats.data.Validated.Invalid
+import cats.data.NonEmptyList
+import io.github.sdev.mpf.domain.entities.AccountValidation
 
-class CreateAccountService[F[_]](accountRepository: AccountRepository[F])(implicit ae: ApplicativeError[F, String])
-    extends CreateAccountUseCase[F]:
+class CreateAccountService[F[_]](accountRepository: AccountRepository[F])(implicit
+    ae: ApplicativeError[F, NonEmptyList[AccountValidation]]
+) extends CreateAccountUseCase[F]:
 
   override def createAccount(account: Account): F[Account] =
     account.toValidated match
       case Valid(account)     => accountRepository.save(account)
-      case Invalid(errorList) => ae.raiseError(errorList.toString) // TODO look for cleanest way to consolidate errors
+      case Invalid(errorList) => ae.raiseError(errorList) // TODO look for cleanest way to consolidate errors
